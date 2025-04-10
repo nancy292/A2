75fb4aee5284d235907989801b872a8f0978035c
nancy292 <nancyreddy292@gmail.com>
2025-04-10 12:16:30 -0400

    Create a task baord

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ templates = Jinja2Templates(directory="templates")
 class EmailRequest(BaseModel):
     email: str
 
+
 # Root route
 @app.get("/", response_class=HTMLResponse)
 def get_login(request: Request):
@@ -42,3 +43,46 @@ async def create_user(data: EmailRequest):
 
     return {"message": "User created"}
 
+
+
+
+
+@app.delete("/taskboard/{boardId}")
+async def delete_taskboard(boardId: str):
+    # Delete the document from Firestore
+    firestore_db.collection("taskboards").document(boardId).delete()
+    return {"board_deleted": True}
+
+
+@app.post("/adduserstotask")
+async def create_user(data: EmailRequest):
+    print("email is", data.email)
+    try:
+        does_team_exist = firestore_db.collection('users').where('email','==',data.email).limit(1).get()
+        userDetails = {"email":data.email}
+        if(len(does_team_exist) == 0):
+            # print("user does not exist")
+            firestore_db.collection('users').add(userDetails)
+        else:
+            # print("User already exist ")
+            pass
+    except Exception as e:
+        print("An error occurred:", e)
+
+    return {"message": "User created"}
+
+
+@app.get("/taskboard", response_class=HTMLResponse)
+def get_login(request: Request):
+    return templates.TemplateResponse("taskboard.html", {"request": request})
+
+
+
+
+@app.post("/addtaskboard/create")
+async def create_taskboard(taskboard: TaskBoard):
+    print("taskboard in create",taskboard)
+    taskboard_dict = taskboard.dict()   
+    board_id = str(uuid4()) 
+    firestore_db.collection("taskboards").document(board_id).set(taskboard_dict)
+    return {"message": "TaskBoard created", "id": board_id}
