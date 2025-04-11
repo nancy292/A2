55dbca42e79580e84bdff38764207fee3bfcae51
nancy292 <nancyreddy292@gmail.com>
2025-04-11 16:04:46 -0400

    Updated UI

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-
-
-# Root route
+# Landing page route
 @app.get("/", response_class=HTMLResponse)
 def get_landingpage(request: Request):
     return templates.TemplateResponse("landingpage.html", {"request": request})
 
 
+
+#Checks if user exist if not exist then creates one 
 @app.post("/CheckAndCreateUser")
 async def create_user(data: EmailRequest):
     print("email is", data.email)
@@ -40,7 +40,7 @@ async def create_user(data: EmailRequest):
 
     return {"message": "User created"}
 
-
+#Adding user to a task 
 @app.post("/adduserstotask")
 async def add_user_to_task(data: EmailRequest):
     print("email is", data.email)
@@ -57,11 +57,12 @@ async def add_user_to_task(data: EmailRequest):
 
     return {"message": "User created"}
 
-
+#View taskboard page
 @app.get("/taskboard", response_class=HTMLResponse)
 def get_taskboard_page(request: Request):
     return templates.TemplateResponse("taskboard.html", {"request": request})
 
+#add a taskbaord to database
 @app.post("/addtaskboard/create")
 async def create_taskboard(taskboard: TaskBoard):
     print("taskboard in create",taskboard)
@@ -70,29 +71,15 @@ async def create_taskboard(taskboard: TaskBoard):
     firestore_db.collection("taskboards").document(board_id).set(taskboard_dict)
     return {"message": "TaskBoard created", "id": board_id}
 
+#fetch all the users
 @app.get("/getallusers")
 async def get_all_users(request:Request):
-    # Fetch all users from the Firestore collection
     users_ref = firestore_db.collection("users")
     users = [doc.to_dict() for doc in users_ref.stream()]
     print("users inside main ",users)
     return {"users": users}
 
-@app.get("/assignedTaskboards/{useremail}")
-async def get_assigned_taskboard(useremail: str):
-    user_tasks = []
-    # Query Firestore for all taskboards
-    taskboards_ref = firestore_db.collection("taskboards")  # Replace "taskboards" with your collection name
-    # Get taskboards documents
-    taskboards = taskboards_ref.stream()
-    for doc in taskboards:
-        taskboard_data = doc.to_dict()
-        if useremail in taskboard_data.get("members", []):
-            taskboard_data["board_id"] = doc.id  # Optionally include Firestore doc ID
-            user_tasks.append(taskboard_data)
-    print("assigned task baords ",user_tasks)
-    return {"user_tasks": user_tasks}
-
+#Check task board name duplicates
 @app.post("/checktaskboardname")
 async def check_boardName(data: NameRequest):
     try:
@@ -107,10 +94,10 @@ async def check_boardName(data: NameRequest):
 
     return {"message": "Task board name is available"}
 
+#Check task name duplicates
 @app.post("/checktaskname")
 async def check_task_name(data: TaskNameRequest):
     try:
-        # Fetch the task board by ID
         task_board_doc = firestore_db.collection('taskboards').document(data.boardId).get()
 
         if not task_board_doc.exists:
@@ -118,7 +105,6 @@ async def check_task_name(data: TaskNameRequest):
 
         task_board_data = task_board_doc.to_dict()
 
-        # Check for existing task title (case-insensitive)
         for task in task_board_data.get("tasks", []):
             if task.get("title", "").lower() == data.task_title.lower():
                 raise HTTPException(status_code=400, detail="Task with the same name already exists in this task board")
@@ -131,24 +117,20 @@ async def check_task_name(data: TaskNameRequest):
 
     return {"message": "Task title is available in this board"}
 
+#Get individual task board
 @app.get("/getIndividualTaskBoard/{board_id}")
 async def get_individual_taskboard(board_id ,request: Request):
-    # print("inside function")
     doc_ref = firestore_db.collection("taskboards").document(board_id)
-    # print("board id ", board_id)
     doc = doc_ref.get()
     taskboard_data = None
-    # print("taskboard ",taskboard_data)
     if doc.exists:
         taskboard_data = doc.to_dict()
-        # print("Taskboard:", taskboard_data)
     else:
-        # print("No such taskboard with ID:", board_id)
         pass
     
     return {"taskboard_data": taskboard_data}
 
-
+#Update task baord 
 @app.put("/taskboard/{boardId}")
 async def edit_taskboard(taskboard: TaskBoard,boardId):
     print("taskbaord ",taskboard,boardId ,type(taskboard))
@@ -156,7 +138,7 @@ async def edit_taskboard(taskboard: TaskBoard,boardId):
     firestore_db.collection("taskboards").document(boardId).update(updatedTaskBoard)
     return {"board_updated":True}
 
-
+#get task baord using email
 @app.get("/taskboards/{useremail}")
 async def get_taskboard_of_user(useremail: str):
     user_tasks = []
@@ -167,7 +149,6 @@ async def get_taskboard_of_user(useremail: str):
         taskboard_data = taskboard.to_dict()
         board_id = taskboard.id
 
-        # Check if user is creator or a member of the board
         if useremail == taskboard_data.get("createdBy") or useremail in taskboard_data.get("members", []):
             taskboard_data["board_id"] = board_id
             user_tasks.append(taskboard_data)
@@ -175,15 +156,14 @@ async def get_taskboard_of_user(useremail: str):
     return {"user_tasks": user_tasks}
 
 
-
+#View individual task board
 @app.get("/viewIndividualTaskBoard/{board_id}")
 async def get_taskboard_using_id(board_id ,request: Request):
     return templates.TemplateResponse("viewIndividualTaskBoard.html", {"request": request, "board_id": board_id})
 
-
+# Delete task board from Firestore
 @app.delete("/taskboard/{boardId}")
 async def delete_taskboard(boardId: str):
-    # Delete the document from Firestore
     firestore_db.collection("taskboards").document(boardId).delete()
     return {"board_deleted": True}
 
